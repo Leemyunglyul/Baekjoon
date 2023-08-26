91cc3afa30fd3498d983be3207783db6a096e21f
Leemyunglyul <140343899+Leemyunglyul@users.noreply.github.com>
2023-08-26 09:36:36 +0900

    mathematics
    https://www.acmicpc.net/problem/1011

---
+++ Baekjoon #1011.c
@@ -0,0 +1,41 @@
+
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+long long arr[47000];
+
+int main()
+{
+    long long a, n, least, x, y, i ,T;
+    scanf("%d", &T);
+    for (i = 0; i < 47000; i++)
+        arr[i] = i * (i + 1);
+    for (i = 0; i < T; i++)
+    {
+        scanf("%lld %lld", &x, &y);
+        a = y - x;
+        n = 0;
+        while (1)
+        {
+            if (a > arr[n] && a <= arr[n+1])
+            {
+                if (a > (n + 1) * (n + 1))
+                {
+                    least = 2*(n+1);
+                    break;
+                }
+                else
+                {
+                    least = 2 * (n + 1)-1;
+                    break;
+                }
+            }
+            n++;
+        }
+        printf("%lld\n", least);
+    }
+}
