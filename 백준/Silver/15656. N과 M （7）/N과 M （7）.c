2e6154f122b85b76e09e57cd90bf2e0c1033205a
Leemyunglyul <140343899+Leemyunglyul@users.noreply.github.com>
2023-09-09 13:33:34 +0900

    [Silver III] Title: N과 M (7), Time: 668 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.c
@@ -0,0 +1,37 @@
+int static compare(const void* first, const void* second)
+{
+	if (*(int*)first > *(int*)second)
+		return 1;
+	else if (*(int*)first < *(int*)second)
+		return -1;
+	else
+		return 0;
+}
+
+int arr[9];
+int p[9];
+int N, M;
+
+int backtracking(int idx) {
+	if (idx == M) {
+		for (int i = 0; i < M; i++)
+			printf("%d ", p[i]);
+		printf("\n");
+		return;
+	}
+
+	for (int i = 0; i < N; i++) {
+		p[idx] = arr[i];
+		backtracking(idx + 1);
+	}
+}
+
+int main() {
+	int i;
+	scanf("%d %d", &N, &M);
+	for (i = 0; i < N; i++)
+		scanf("%d", &arr[i]);
+	qsort(arr, N, sizeof(int), compare);
+	backtracking(0);
+	return 0;
+}
