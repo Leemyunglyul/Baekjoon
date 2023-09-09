bfa02f1e1fc4f049dc8ab94ba7adf79bd203644b
Leemyunglyul <140343899+Leemyunglyul@users.noreply.github.com>
2023-09-09 15:10:49 +0900

    [Gold V] Title: Fly me to the Alpha Centauri, Time: 0 ms, Memory: 1480 KB -BaekjoonHub

---
+++ 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.c
@@ -0,0 +1,33 @@
+long long arr[47000];
+
+int main()
+{
+    long long a, n, least, x, y, i ,T;
+    scanf("%d", &T);
+    for (i = 0; i < 47000; i++)
+        arr[i] = i * (i + 1);
+    for (i = 0; i < T; i++)
+    {
+        scanf("%lld %lld", &x, &y);
+        a = y - x;
+        n = 0;
+        while (1)
+        {
+            if (a > arr[n] && a <= arr[n+1])
+            {
+                if (a > (n + 1) * (n + 1))
+                {
+                    least = 2*(n+1);
+                    break;
+                }
+                else
+                {
+                    least = 2 * (n + 1)-1;
+                    break;
+                }
+            }
+            n++;
+        }
+        printf("%lld\n", least);
+    }
+}
