b40ac70807f2c36ac2eb42930986de33ca936a11
Leemyunglyul <140343899+Leemyunglyul@users.noreply.github.com>
2023-09-17 20:07:57 +0900

    [Gold IV] Title: 스도쿠, Time: 88 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.cc
+++ 백준/Gold/2580. 스도쿠/스도쿠.cc
@@ -1,135 +1,58 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <cstdio>
-#include <vector>
-#include <cmath>
-#include <algorithm>
-
 using namespace std;
-
-int sudoku[9][9];
-int s[81];
-int BLANK[81];
-int k=0;
-
-int check(int n, int m)
-{
-	int i, j;
-	for (i = 0; i < 9; i++)
-		if ((sudoku[n][m] == sudoku[i][m] && i != n) || (sudoku[n][m] == sudoku[n][i] && i != m))
-			return 0;
-	for (i = (n / 3) * 3; i < (n / 3) * 3 + 3; i++)
-	{
-		for(j=(m/3)*3;j< (m / 3) * 3+3;j++)
-			if (sudoku[i][j] == sudoku[n][m] && n != i && m!=j)
-				return 0;
-	}
-	return 1;
+int a[10][10];
+bool c[10][10];
+bool c2[10][10];
+bool c3[10][10];
+int n=9;
+int cnt=0;
+int square(int x, int y) {
+    return (x/3)*3+(y/3);
 }
-
-void blank(int X)
-{
-	int n = s[X] / 10;
-	int m = s[X] % 10;
-	int num=0;
-	int i, j;
-	for (i = 0; i < 9; i++)
-		if (sudoku[i][m] == 0 && i != n) 
-			num++;
-	for (i = 0; i < 9; i++)
-		if (sudoku[n][i] == 0 && i != m)
-			num++;
-	for (i = (n / 3) * 3; i < (n / 3) * 3 + 3; i++)
-	{
-		for (j = (m / 3) * 3; j < (m / 3) * 3 + 3; j++)
-			if (sudoku[i][j] == 0 && !(n == i || m == j))
-				num++;
-	}
-	BLANK[X] = num;
+bool go(int z) {
+    cnt += 1;
+    if (cnt >= 10000000) {
+        return true;
+    }
+    if (z == 81) {
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n; j++) {
+                cout << a[i][j] << ' ';
+            }
+            cout << '\n';
+        }
+        return true;
+    }
+    int x = z/n;
+    int y = z%n;
+    if (a[x][y] != 0) {
+        return go(z+1);
+    } else {
+        for (int i=1; i<=9; i++) {
+            if (c[x][i] == 0 && c2[y][i] == 0 && c3[square(x,y)][i]==0) {
+                c[x][i] = c2[y][i] = c3[square(x,y)][i] = true;
+                a[x][y] = i;
+                if (go(z+1)) {
+                    return true;
+                }
+                a[x][y] = 0;
+                c[x][i] = c2[y][i] = c3[square(x,y)][i] = false;
+            }
+        }
+    }
+    return false;
 }
-
-void backtracking(int x, int * num)
-{
-	if (*num == 1)
-		return;
-	int a = s[x] / 10;
-	int b = s[x] % 10;
-	for (int i = 1; i <= 9; i++)
-	{
-		sudoku[a][b] = i;
-		if (check(a, b) == 0)
-		{
-			//printf("아니었다! %d %d: 현재위치 %d 현재 숫자 %d[%d]\n", a, b, x, i,*num);
-			continue;
-		}
-		if (x == k-1)
-		{
-			//printf("돌아간다 %d->%d\n", x, k);
-			*num = 1;
-			return;
-		}
-		//printf("일단 이걸로%d %d: 현재위치 %d 현재 숫자 %d[%d]\n", a, b, x, i, *num);
-		backtracking(x + 1, num);
-		if (*num == 1)
-			return;			
-	}
-	sudoku[a][b] = 0;
-}
-
-void quickSort(int arr[], int L, int R) {
-	int left = L, right = R;
-	int pivot = arr[(L + R) / 2];    // pivot 설정 (가운데) 
-	int temp;
-	do
-	{
-		while (arr[left] < pivot)    // left가 pivot보다 큰 값을 만나거나 pivot을 만날 때까지 
-			left++;
-		while (arr[right] > pivot)    // right가 pivot보다 작은 값을 만나거나 pivot을 만날 때까지 
-			right--;
-		if (left <= right)    // left가 right보다 왼쪽에 있다면 교환 
-		{
-			temp = arr[left];
-			arr[left] = arr[right];
-			arr[right] = temp;
-			temp = s[left];
-			s[left] = s[right];
-			s[right] = temp;
-			/*left 오른쪽으로 한칸, right 왼쪽으로 한칸 이동*/
-			left++;
-			right--;
-		}
-	} while (left <= right);    // left가 right 보다 오른쪽에 있을 때까지 반복 
-
-	/* recursion */
-	if (L < right)
-		quickSort(arr, L, right);    // 왼쪽 배열 재귀적으로 반복 
-
-	if (left < R)
-		quickSort(arr, left, R);    // 오른쪽 배열 재귀적으로 반복 
-}
-
-int main()
-{
-	int i, j, num;
-	for (i = 0; i < 9; i++) {
-		for (j = 0; j < 9; j++){
-			cin >> sudoku[i][j];
-			if (sudoku[i][j] == 0)
-				s[k++] = 10 * i + j;
-		}
-	}
-	//for (i = 0; i < k; i++)
-		//blank(i);
-	//quickSort(BLANK ,0, k - 1);
-	//for (i = 0; i < k; i++)
-		//printf("(%d %d)\n", s[i], BLANK[i]);
-	num = 0;
-	backtracking(0, &num);
-	for (i = 0; i < 9; i++) {
-		printf("%d", sudoku[i][0]);
-		for (j = 1; j < 9; j++) {
-			printf(" %d", sudoku[i][j]);
-		}
-		printf("\n");
-	}
+int main() {
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<n; j++) {
+            cin >> a[i][j];
+            if (a[i][j] != 0) {
+                c[i][a[i][j]] = true;
+                c2[j][a[i][j]] = true;
+                c3[square(i,j)][a[i][j]] = true;
+            }
+        }
+    }
+    go(0);
+    return 0;
 }
