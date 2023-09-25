c655dc53d5c7d393dcc758368363c24af5f55bc7
Leemyunglyul <140343899+Leemyunglyul@users.noreply.github.com>
2023-09-26 00:21:23 +0900

    [Bronze IV] Title: 운동장 한 바퀴, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16486. 운동장 한 바퀴/운동장 한 바퀴.cc
@@ -0,0 +1,17 @@
+//#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+//#include <string>
+//#include <vector>
+//#include <cmath>
+//#include <queue>
+#include <cstdio>
+
+using namespace std;
+
+int main()
+{
+	int a, b;
+	cin >> a >> b;
+	double c = 2 * a + (b * 2 * 3.1415926535);
+	printf("%.6lf", c);
+}
