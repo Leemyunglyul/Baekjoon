a1e49281653ef6b6624b9892cc74b025be44f265
Leemyunglyul <140343899+Leemyunglyul@users.noreply.github.com>
2023-10-15 00:02:18 +0900

    [Bronze III] Title: 택시 기하학, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3053. 택시 기하학/택시 기하학.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <queue>
+
+using namespace std;
+
+
+int main() {
+	double n;
+	cin >> n;
+	cout << fixed;
+	cout.precision(6);
+	cout << n * n * 3.141592653589 << endl << n * n * 2;
+}
