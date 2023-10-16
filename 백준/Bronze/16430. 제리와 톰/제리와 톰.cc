0d60d192ae3da6a4253748a30cf9f9cc90259178
Leemyunglyul <140343899+Leemyunglyul@users.noreply.github.com>
2023-10-16 17:56:18 +0900

    [Bronze V] Title: 제리와 톰, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16430. 제리와 톰/제리와 톰.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <cmath>
+#include <queue>
+
+using namespace std;
+
+int main() {
+	int x, y;
+	cin >> x>>y;
+	cout << y - x << " " << y;
+}
