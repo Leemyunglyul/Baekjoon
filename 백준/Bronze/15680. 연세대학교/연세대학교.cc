5f59ee4c18d04ebf42a9ced9e5a3692baa4ee5ad
Leemyunglyul <140343899+Leemyunglyul@users.noreply.github.com>
2023-10-16 17:33:48 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <cmath>
+#include <queue>
+
+using namespace std;
+
+int main() {
+	int n, i, sum;
+	cin >> n;
+	if (n == 0) cout << "YONSEI";
+	else cout << "Leading the Way to the Future";
+}
