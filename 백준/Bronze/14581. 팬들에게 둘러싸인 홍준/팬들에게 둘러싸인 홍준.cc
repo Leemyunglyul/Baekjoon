adb4d5c128d516365d77984267b32e3344c53849
Leemyunglyul <140343899+Leemyunglyul@users.noreply.github.com>
2023-10-16 17:53:15 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <cmath>
+#include <queue>
+
+using namespace std;
+
+int main() {
+	string x;
+	cin >> x;
+	cout << ":fan::fan::fan:"<<endl<<":fan::"<<x<<"::fan:"<<endl<<":fan::fan::fan:";
+}
