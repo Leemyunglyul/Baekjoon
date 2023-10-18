03ca0f0c48d6e38241ac7a58fb4ef4fe29becf21
Leemyunglyul <140343899+Leemyunglyul@users.noreply.github.com>
2023-10-18 23:34:45 +0900

    [Bronze V] Title: Can you add this?, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7891. Can you add this？/Can you add this？.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <cmath>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+int main() {
+	int n, i, x, y;
+	cin >> n;
+	for (i = 0; i < n; i++) {
+		cin >> x >> y;
+		cout<< x + y << endl;
+	}
+}
