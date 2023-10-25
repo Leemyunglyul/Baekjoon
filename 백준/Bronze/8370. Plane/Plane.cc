cdd073cf3483d5d0f0c7dc12eb5e469589d7a410
Leemyunglyul <140343899+Leemyunglyul@users.noreply.github.com>
2023-10-25 16:31:53 +0900

    [Bronze V] Title: Plane, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8370. Plane/Plane.cc
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <cmath>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+int main() {
+	int a, b, c, d;
+	cin >> a >> b >> c >> d;
+	cout << a * b + c * d;
+}
