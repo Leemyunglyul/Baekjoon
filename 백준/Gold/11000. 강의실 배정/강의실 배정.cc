0de5d0d3dd4df52cb036a77ad1705dfcc4a56cce
Leemyunglyul <140343899+Leemyunglyul@users.noreply.github.com>
2023-11-04 23:28:19 +0900

    [Gold V] Title: 강의실 배정, Time: 204 ms, Memory: 5108 KB -BaekjoonHub

---
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.cc
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <cmath>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+priority_queue <int, vector<int>, greater<int>> p;
+
+int main() {
+	int n, i, x,y;
+	cin >> n;
+	while (n--) {
+		cin >> x >> y;
+		pq.push({ x, y });
+	}
+	while (pq.empty() == 0) {
+		if (p.empty() == 1) {
+			p.push(pq.top().second);
+		}
+		else {
+			if (p.top() <= pq.top().first) {
+				p.pop();
+			}
+			p.push(pq.top().second);
+		}
+		pq.pop();
+	}
+	cout << p.size();
+}
