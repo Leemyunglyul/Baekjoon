59a2804294c089486b09da444cd168d75a3de26f
Leemyunglyul <140343899+Leemyunglyul@users.noreply.github.com>
2023-11-27 23:30:04 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 336 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.cc
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstdio>
+//#include <string>
+#include <cmath>
+//#include <queue>
+//#include <stack>
+#include <algorithm>
+
+using namespace std;
+int arr[1000002];
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int n, i;
+	cin >> n;
+	for (i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	sort(arr, arr + n);
+	for (i = n-1; i >=0; i--) {
+		printf("%d\n", arr[i]);
+	}
+}
