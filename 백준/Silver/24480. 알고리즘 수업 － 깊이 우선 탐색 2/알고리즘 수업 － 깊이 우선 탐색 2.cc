246935d86f6b6dd3cfb14b1303fba8fa7619d409
Leemyunglyul <140343899+Leemyunglyul@users.noreply.github.com>
2024-01-02 08:55:29 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 112 ms, Memory: 11184 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.cc
@@ -0,0 +1,49 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <map>
+#include <functional>
+//#include <set>
+using namespace std;
+
+vector<int> graph[100001];
+int visited[100001] = { 0, };
+int result[100001];
+int cnt = 0;
+
+void dfs(int r) {
+	if (visited[r] == 1) { 
+		return;
+	}
+
+	visited[r] = 1; 
+	cnt++;
+	result[r] = cnt;
+
+	for (int i = 0; i < graph[r].size(); i++) {
+		dfs(graph[r][i]);
+	}
+}
+int main() {
+	int n, m, r;
+	scanf("%d %d %d", &n, &m, &r);
+	for (int i = 1; i <= m; i++) {
+		int a, b;
+		scanf("%d %d", &a, &b);
+		graph[a].push_back(b); 
+		graph[b].push_back(a); 
+	}
+	for (int i = 1; i <= n; i++) {
+			sort(graph[i].begin(), graph[i].end(), greater<>());
+	}
+	dfs(r);
+	for (int i = 1; i <= n; i++) {
+		printf("%d\n", result[i]);
+	}
+
+}
