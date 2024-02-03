c61a6c50c5c2ae21e9f44af4d0730aa834d73b3d
Leemyunglyul <140343899+Leemyunglyul@users.noreply.github.com>
2024-02-03 20:22:50 +0900

    [Bronze III] Title: 그대로 출력하기 2, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.cc
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<math.h>
+#include<string>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	string x;
+	while (getline(cin, x)) {
+		cout << x << endl;
+	}
+}
