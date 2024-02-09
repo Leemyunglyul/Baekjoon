a994e40691cca7a32770ba4815957d45cd97d7ad
Leemyunglyul <140343899+Leemyunglyul@users.noreply.github.com>
2024-02-09 17:04:19 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int n;
+	cin >> n;
+	if (n % 2 == 0) cout << "CY";
+	else cout << "SK";
+}
