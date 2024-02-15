7af9dcb63599bccb686d625ada8c495e8d741683
Leemyunglyul <140343899+Leemyunglyul@users.noreply.github.com>
2024-02-16 00:04:30 +0900

    [Bronze IV] Title: 기찍 N, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze IV/2742. 기찍 N/기찍 N.cc
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int n;
+	cin >> n;
+	while (n > 0) cout << n-- << "\n";
+}
