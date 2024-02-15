8dab519c77bba770c3e9cc9818bf673612976b26
Leemyunglyul <140343899+Leemyunglyul@users.noreply.github.com>
2024-02-15 20:12:15 +0900

    [Gold V] Title: 암호코드, Time: 0 ms, Memory: 2104 KB -BaekjoonHub

--- 백준/Gold V/2011. 암호코드/암호코드.cc
+++ 백준/Gold V/2011. 암호코드/암호코드.cc
@@ -14,16 +14,13 @@ int main() {
 	int i, j, temp;
 	for (i = 0; i < x.size(); i++) {
 		if (x[i] - 48 > 0) dp1[i]++;
-		//cout << dp1[i] << "//";
-	}//cout << endl;
+	}
 	for (i = 0; i < x.size() - 1; i++) {
 		temp = (x[i] - 48) * 10 + x[i + 1] - 48;
-		if (x[i]!=48 && temp >= 1 && temp <= 26) dp2[i]++;
-		//cout << dp2[i] << "//";
-	}//cout << endl;
+		if (x[i] != 48 && temp >= 1 && temp <= 26) dp2[i]++;
+	}
 	for (i = 2, sum[0] = dp1[0], sum[1] = sum[0] * dp1[1] + dp2[0]; i < x.size(); i++) {
 		sum[i] = (sum[i - 1] * dp1[i] + sum[i - 2] * dp2[i - 1]) % 1000000;
-		//cout << sum[i] << "//";
-	}//cout << endl;
+	}
 	cout << sum[x.size() - 1];
 }
