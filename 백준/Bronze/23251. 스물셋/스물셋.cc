435b2666ae1c329efe42d4feb1521ab40d5a230a
Leemyunglyul <140343899+Leemyunglyul@users.noreply.github.com>
2024-03-20 15:40:20 +0900

    [Bronze II] Title: 스물셋, Time: 52 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23251. 스물셋/스물셋.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include<cmath>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    long long int testn, x;
+    cin >> testn;
+    while (testn--) {
+        cin >> x;
+        cout << 23 * x << "\n";
+    }
+}
