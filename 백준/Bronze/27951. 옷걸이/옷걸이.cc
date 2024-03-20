e8328f2c3ae7193d270e370f9a99d1c3fc4c72de
Leemyunglyul <140343899+Leemyunglyul@users.noreply.github.com>
2024-03-20 20:28:06 +0900

    [Bronze I] Title: 옷걸이, Time: 228 ms, Memory: 6184 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/27951. 옷걸이/옷걸이.cc
+++ 백준/Bronze/27951. 옷걸이/옷걸이.cc
@@ -1,43 +1,64 @@
 #include <iostream>
-#include<cmath>
+#include <queue>
 
 using namespace std;
 
-int hang[1000010];
-int arr[4] = { 0 };
+int main()
+{
+    int i,n;
+    int hanger[4]={0,};
+    queue<int> hanger_type;
+    int u,d;
+    int uin1, uin3, din2, din3;
 
-int main() {
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    int hn, up, down, i;
-    cin >> hn;
-    for (i = 1; i <= hn; i++) {
-        cin >> hang[i];
-        arr[hang[i]]++;
+    cin >> n;
+    for(i=1;i<=n;i++){
+        int temp;
+        cin >> temp;
+        hanger_type.push(temp);
+        hanger[temp]++;
     }
-    cin >> up >> down;
-    if (up > arr[1] + arr[3] || down>arr[2]+arr[3]) {
-        cout << "NO";
-        return 0;
+    cin >> u >> d;
+
+    if(hanger[1]>=u) uin1=u;
+    else{
+        uin1 = hanger[1];
+        if(hanger[3]>=(u-hanger[1])) uin3 = (u-hanger[1]);
+        else{
+            cout << "NO";
+            return 0;
+        }
     }
-    cout << "YES\n";
-    for (i = 1; i <= hn; i++) {
-        if (hang[i] == 1) {
-            up--;
-            cout << "U";
+
+    if(hanger[2]>=d) din2=d;
+    else{
+        din2 = hanger[2];
+        if(hanger[3]>=(d-hanger[2])) din3 = (d-hanger[2]);
+        else{
+            cout << "NO";
+            return 0;
         }
-        else if (hang[i] == 2) {
-            down--;
-            cout << "D";
-        }
-        else {
-            if (up > 0) {
-                up--;
+    }
+
+    cout << "YES"<< endl;
+    for(i=1;i<=n;i++){
+        int temp = hanger_type.front();
+        hanger_type.pop();
+        if(temp==3){
+            if(uin3){
+                uin3--;
                 cout << "U";
             }
-            else {
-                down--;
+            else if(din3){
+                din3--;
                 cout << "D";
             }
         }
+        else{
+            if(temp==2) cout << "D";
+            else cout << "U";
+        }
     }
+
+    return 0;
 }
