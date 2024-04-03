65ccb4b280bf4f0cd34c114e396328806f386c70
Leemyunglyul <140343899+Leemyunglyul@users.noreply.github.com>
2024-04-03 16:28:00 +0900

    [Platinum III] Title: 물자 조달, Time: 324 ms, Memory: 23100 KB, Score: 80 point -BaekjoonHub

--- 백준/Platinum/31268. 물자 조달/물자 조달.cc
+++ 백준/Platinum/31268. 물자 조달/물자 조달.cc
@@ -12,20 +12,21 @@ long long dp[210][210];
 long long stop[210];
 vector<tuple<int, int, int>> query;
 stack<long long> anw;
-int n, m, q;
+long long n, m, q;
 
 int main() {
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
     cin >> n >> m >> q;
     long long i, j, x, y, w, k, z;
+    fill_n(&dp[0][0], 210 * 210, big);
     for (i = 1; i <= n; i++) {
         cin >> stop[i];
+        dp[i][i] = 0;
     }
-    fill_n(&dp[0][0], 210 * 210, big);
     for (i = 1; i <= m; i++) {
         cin >> x >> y >> w;
-        dp[x][y] = w;
-        dp[y][x] = w;
+        dp[x][y] = min(dp[x][y], w);
+        dp[y][x] = min(dp[y][x], w);
     }
     for (i = 1; i <= q; i++) {
         cin >> x >> y >> z;
@@ -37,24 +38,19 @@ int main() {
     for (k = 1; k <= n; k++) {
         for (i = 1; i <= n; i++) {
             for (j = 1; j <= n; j++) {
-                if (dp[i][j] > dp[i][k] + dp[k][j] + stop[k]) {
-                    dp[i][j] = dp[i][k] + dp[k][j] + stop[k];
-                }
+                dp[i][j] = min(dp[i][j], dp[i][k] + dp[k][j] + stop[k]);
             }
         }
     }
-    for (i = q-1; i >=0; i--) {
+    for (i = q - 1; i >= 0; i--) {
         x = get<0>(query[i]);
         y = get<1>(query[i]);
-        z= get<2>(query[i]);
+        z = get<2>(query[i]);
         if (x == 1) {
             stop[y] -= z;
             for (k = 1; k <= n; k++) {
-                if (dp[k][y] == big) continue;
                 for (j = 1; j <= n; j++) {
-                    if (dp[k][j] > dp[k][y] + dp[y][j] + stop[y]) {
-                        dp[k][j] = dp[k][y] + dp[y][j] + stop[y];
-                    }
+                    dp[k][j] = min(dp[k][j], dp[k][y] + dp[y][j] + stop[y]);
                 }
             }
         }
@@ -64,7 +60,7 @@ int main() {
         }
     }
     while (!anw.empty()) {
-        cout << anw.top()<<"\n";
+        cout << anw.top() << "\n";
         anw.pop();
     }
 }
