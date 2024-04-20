c502aea41ab43aa3f0bc8c2dfff743cb7339148c
Leemyunglyul <140343899+Leemyunglyul@users.noreply.github.com>
2024-04-20 14:33:14 +0900

    [Silver III] Title: 잘 알려진 수열 구하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/24516. 잘 알려진 수열 구하기/잘 알려진 수열 구하기.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+#include <queue>
+#include <vector>
+#include <algorithm>
+#include <tuple>
+#include <string>
+#include <map>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int n, i;
+    cin >> n;
+    for (i = 1; i <= n; i++) {
+        cout << i * 2 << " ";
+    }
+
+    return 0;
+}
