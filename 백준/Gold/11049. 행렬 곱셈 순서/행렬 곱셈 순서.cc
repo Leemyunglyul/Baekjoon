e8771be758eabf1c82e333232c55b01176ec8790
Leemyunglyul <140343899+Leemyunglyul@users.noreply.github.com>
2024-04-23 23:37:07 +0900

    [Gold III] Title: 행렬 곱셈 순서, Time: 48 ms, Memory: 4056 KB -BaekjoonHub

---
+++ 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cmath>
+#include <queue>
+#include <vector>
+#include <algorithm>
+#include <tuple>
+#include <string>
+#include <map>
+
+using namespace std;
+
+int mat[510][2], n;
+long long dp[510][510];
+long long big = 987654321987;
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    cin >> n;
+    int i, s, m, e;
+    for (i = 1; i <= n; i++) {
+        cin >> mat[i][0] >> mat[i][1];
+        
+    }
+    fill_n(&dp[0][0], 510 * 510, big);
+    for (i = 1, dp[n][n]=0; i < n; i++) {
+        dp[i][i + 1] = mat[i][0] * mat[i][1] * mat[i + 1][1];
+        dp[i][i] = 0;
+    }
+    for (i = 1; i <= n-1; i++) {
+        for (s = 1; s <= n; s++) {//start
+            e = s + i;
+            if (e > n) break;
+            for (m = s; m < e; m++) {//end
+                dp[s][e] = min(dp[s][e],
+                    dp[s][m]+dp[m+1][e]+mat[s][0]*mat[m][1]*mat[e][1]);
+            }
+       }
+    }
+    cout << dp[1][n];
+
+    return 0;
+}
