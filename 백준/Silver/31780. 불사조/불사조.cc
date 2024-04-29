673471cd1311814c3688f20f42b1619ecb6f0082
Leemyunglyul <140343899+Leemyunglyul@users.noreply.github.com>
2024-04-29 13:02:00 +0900

    [Silver IV] Title: 불사조, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/31780. 불사조/불사조.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+#include <queue>
+#include <vector>
+#include <algorithm>
+#include <tuple>
+#include <string>
+#include <map>
+#include <set>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    long long n, m;
+    cin >> n >> m;
+    cout << n * (m + 1);
+    
+
+    return 0;
+}
