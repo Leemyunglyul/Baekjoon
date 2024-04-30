251128fb6563da47a85fb0360dbb4b6491650540
Leemyunglyul <140343899+Leemyunglyul@users.noreply.github.com>
2024-04-30 09:05:10 +0900

    [Bronze II] Title: 그릇, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/7567. 그릇/그릇.cc
+++ 백준/Bronze/7567. 그릇/그릇.cc
@@ -1,17 +1,6 @@
 #include <iostream>
-#include <cmath>
-#include <queue>
-#include <vector>
-#include <algorithm>
-#include <tuple>
-#include <string>
-#include <map>
-#include <set>
-
 using namespace std;
-
 int main() {
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
     string x;
     int i, sum;
     cin >> x;
