d8fda3cc1a646b33e8640c82959d9c9a2696c5b5
Leemyunglyul <140343899+Leemyunglyul@users.noreply.github.com>
2024-07-30 14:58:25 +0900

    [Gold III] Title: 줄 세우기, Time: 32 ms, Memory: 4152 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.cc
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.cc
@@ -1,44 +1,40 @@
-#include<iostream>
-#include<math.h>
-#include<queue>
-#include<vector>
+#include <iostream>
+#include <cmath>
+#include <queue>
+#include <vector>
+#include <tuple>
+#include <stack>
+#include <algorithm>
+#include <map>
 
 using namespace std;
 
-int arr[33000] = {0};
-vector<int> print;
-vector<vector<int>> nextt;
+vector<int> edge[32010];
+int arr[32010];
+vector<int> result;
 queue<int> q;
 
 int main() {
-	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-	int n, m, i, x, y;
-	cin >> n >> m;
-	vector<int> temp;
-	temp.push_back(0);
-	nextt.push_back(temp);
-	for (i = 1; i <= n; i++) {
-		nextt.push_back(temp);
-	}
-	temp.clear();
-	for (i = 0; i < m; i++) {
-		cin >> x >> y;
-		nextt[x].push_back(y);
-		arr[y]++;
-	}
-	for (i = 1; i <= n; i++) {//먼저 서도 되는 친구들
-		if (arr[i] == 0) q.push(i);
-	}
-	while (!q.empty()) {
-		x = q.front();
-		q.pop();
-		for (i = 1; i < nextt[x].size(); i++) {//위상 정렬 탐색
-			arr[nextt[x][i]]--;
-			if (arr[nextt[x][i]] == 0) {
-				q.push(nextt[x][i]);
-			}
-		}
-		print.push_back(x);
-	}
-	for (i = 0; i < print.size(); i++) cout << print[i] << " ";
+    ios_base::sync_with_stdio(false);cin.tie(NULL); cout.tie(NULL);
+
+    int n, m, i, j, x, y;
+    cin >> n >> m;
+    for (i = 1; i <= m; i++) {
+        cin >> x >> y;
+        edge[x].push_back(y);
+        arr[y]++;
+    }
+    for (i = 1; i <= n; i++) {
+        if (arr[i] == 0) q.push(i);
+    }
+    while (!q.empty()) {
+        x = q.front();
+        result.push_back(x);
+        q.pop();
+        for (i = 0; i < edge[x].size(); i++) {
+            arr[edge[x][i]]--;
+            if (arr[edge[x][i]] == 0) q.push(edge[x][i]);
+        }
+    }
+    for (i = 0; i < result.size(); i++) cout << result[i] << " ";
 }
