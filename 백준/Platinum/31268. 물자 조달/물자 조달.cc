231f952bf3a5c25bf83149435d7733d5b1740292
Leemyunglyul <140343899+Leemyunglyul@users.noreply.github.com>
2024-08-22 19:08:41 +0900

    [Platinum III] Title: 물자 조달, Time: 296 ms, Memory: 39364 KB, Score: 300 point -BaekjoonHub

--- 백준/Platinum/31268. 물자 조달/물자 조달.cc
+++ 백준/Platinum/31268. 물자 조달/물자 조달.cc
@@ -7,25 +7,26 @@
 
 using namespace std;
 
-long long big = 987654321987654;
+long long big = 187654321987654321;
 long long dp[210][210];
 long long stop[210];
-vector<tuple<int, int, int>> query;
+vector<tuple<long long, long long, long long>> query;
 stack<long long> anw;
 int n, m, q;
 
 int main() {
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
     cin >> n >> m >> q;
-    long long i, j, x, y, w, k, z;
+    long long i, j, x, y, w, k, z, pre;
+    fill_n(&dp[0][0], 210 * 210, big);
     for (i = 1; i <= n; i++) {
         cin >> stop[i];
+        dp[i][i] = 0;
     }
-    fill_n(&dp[0][0], 210 * 210, big);
     for (i = 1; i <= m; i++) {
         cin >> x >> y >> w;
-        dp[x][y] = w;
-        dp[y][x] = w;
+        dp[x][y] = min(w, dp[x][y]);
+        dp[y][x] = min(w, dp[y][x]);
     }
     for (i = 1; i <= q; i++) {
         cin >> x >> y >> z;
@@ -36,25 +37,25 @@ int main() {
     }
     for (k = 1; k <= n; k++) {
         for (i = 1; i <= n; i++) {
-            if (k == i) continue;
             for (j = 1; j <= n; j++) {
-                if (k == j) continue;
                 if (dp[i][j] > dp[i][k] + dp[k][j] + stop[k]) {
                     dp[i][j] = dp[i][k] + dp[k][j] + stop[k];
                 }
             }
         }
     }
-    for (i = q - 1; i >= 0; i--) {
+    for (i = query.size()-1, pre=0; i >= 0; i--) {
         x = get<0>(query[i]);
         y = get<1>(query[i]);
         z = get<2>(query[i]);
         if (x == 1) {
             stop[y] -= z;
+            if(pre==0|| pre==y){
+                pre=y; continue;
+            }
+            swap(pre, y);
             for (k = 1; k <= n; k++) {
-                if (k == y) continue;
                 for (j = 1; j <= n; j++) {
-                    if (y == j) continue;
                     if (dp[k][j] > dp[k][y] + dp[y][j] + stop[y]) {
                         dp[k][j] = dp[k][y] + dp[y][j] + stop[y];
                     }
@@ -62,8 +63,13 @@ int main() {
             }
         }
         else {
-            if (dp[y][z] == big) anw.push(-1);
-            else anw.push(dp[y][z]);
+            if (dp[y][z] >= big) anw.push(-1);
+            else{
+                if(pre==0) anw.push(dp[y][z]);
+                else{
+                    anw.push(min(dp[y][z], dp[y][pre]+dp[pre][z]+stop[pre]));
+                }
+            }
         }
     }
     while (!anw.empty()) {
