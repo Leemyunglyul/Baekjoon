651c1d6e4ccabcbb402d1fd08e108872d2f9d0ec
Leemyunglyul <140343899+Leemyunglyul@users.noreply.github.com>
2024-09-02 15:04:04 +0900

    [Bronze III] Title: What is n, Daddy?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/6794. What is n， Daddy？/What is n， Daddy？.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+#include <queue>
+#include <vector>
+#include <tuple>
+#include <stack>
+#include <set>
+#include <algorithm>
+#include <string>
+#define ll long long
+
+using namespace std;
+
+int arr[11]={0, 1, 2, 2, 3, 3, 3, 2, 2, 1, 1};
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    int n;
+    cin>>n;
+    cout<<arr[n];
+}
