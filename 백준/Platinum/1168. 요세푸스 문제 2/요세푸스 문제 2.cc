d3befd842247f75ee80d1875bbc04957f836a023
Leemyunglyul <140343899+Leemyunglyul@users.noreply.github.com>
2024-09-17 13:00:49 +0900

    [Platinum III] Title: 요세푸스 문제 2, Time: 32 ms, Memory: 6888 KB -BaekjoonHub

--- 백준/Platinum/1168. 요세푸스 문제 2/요세푸스 문제 2.cc
+++ 백준/Platinum/1168. 요세푸스 문제 2/요세푸스 문제 2.cc
@@ -6,7 +6,7 @@ using namespace std;
 long long segt[410000];
 
 long long init(int node, int st, int end){
-    if(st == end) return segt[node] = 1; // 각 리프 노드에 1을 저장 (사람이 1명 존재)
+    if(st == end) return segt[node] = 1;
     int mid = (st + end) / 2;
     return segt[node] = init(node * 2, st, mid) + init(node * 2 + 1, mid + 1, end);
 }
@@ -37,24 +37,18 @@ int main() {
     long long n, k;
     cin >> n >> k;
     
-    init(1, 1, n); // 세그먼트 트리 초기화
+    init(1, 1, n);
     
-    long long t = k; // 처음 시작할 사람의 위치는 k
+    long long t = k;
     for(long long i = 1; i <= n; i++){
-        // 현재 t번째 사람을 찾고 그 사람을 제거
         long long x = find(1, 1, n, t);
         update(1, 1, n, x);
         print.push_back(x);
-
-        // 남은 사람 수가 있는지 확인
         if(segt[1] > 0) {
-            // t를 다음 제거할 위치로 업데이트
             t = (t + k - 1) % segt[1];
-            if(t == 0) t = segt[1]; // 남은 사람 수를 넘어가면 다시 처음으로 돌아옴
+            if(t == 0) t = segt[1]; 
         }
     }
-
-    // 결과 출력
     cout << "<";
     for(long long i = 0; i < print.size(); i++){
         if(i < n - 1) cout << print[i] << ", ";
