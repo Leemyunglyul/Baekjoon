e1d1a2617d7d5b8011061838b357c084552682f3
Leemyunglyul <140343899+Leemyunglyul@users.noreply.github.com>
2024-09-20 22:26:36 +0900

    [Platinum III] Title: 회사 문화 3, Time: 60 ms, Memory: 19380 KB -BaekjoonHub

---
+++ 백준/Platinum/14287. 회사 문화 3/회사 문화 3.cc
@@ -0,0 +1,190 @@
+#include <iostream>
+#include <cmath>
+#include <queue>
+#include <vector>
+#include <tuple>
+#include <stack>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <map>
+
+using namespace std;
+
+vector<int> edge[101000];
+long long tree[410000];
+long long lazy[410000];
+int ss[110000], ee[110000];
+int cnt=0;
+
+void dfs(int now){
+    ss[now]=++cnt;
+    for(int i=0;i<edge[now].size();i++){
+        dfs(edge[now][i]);
+    }
+    ee[now]=cnt;
+   // cout<<now<<"=="<<ss[now]<<"~~"<<ee[now]<<endl;
+}
+
+void update_lazy(int node, int start, int end) {
+    if (lazy[node] != 0) {
+        tree[node] += lazy[node];
+        if (start != end) {
+            lazy[node*2] += lazy[node];
+            lazy[node*2+1] += lazy[node];
+        }
+        lazy[node] = 0;
+    }
+}
+void update_range(int node, int start, int end, int left, int right, long long diff) {
+    update_lazy(node, start, end);
+    if (left > end || right < start) {
+        return;
+    }
+    if (left <= start && end <= right) {
+        tree[node] += diff;
+        if (start != end) {
+            lazy[node*2] += diff;
+            lazy[node*2+1] += diff;
+        }
+        return;
+    }
+    update_range(node*2, start, (start+end)/2, left, right, diff);
+    update_range(node*2+1, (start+end)/2+1, end, left, right, diff);
+    tree[node] = tree[node*2] + tree[node*2+1];
+}
+
+long long query(int node, int start, int end, int left, int right) {
+    update_lazy(node, start, end);
+    if (left > end || right < start) {
+        return 0;
+    }
+    if (left <= start && end <= right) {
+        return tree[node];
+    }
+    long long lsum = query(node*2, start, (start+end)/2, left, right);
+    long long rsum = query(node*2+1, (start+end)/2+1, end, left, right);
+    return lsum + rsum;
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    long long n, m, i, j, x, a, b, c;
+    cin>>n>>m;
+    for(i=1;i<=n;i++){
+        cin>>x;
+        if(x>0){
+            edge[x].push_back(i);
+        }
+    }
+    dfs(1);
+    while(m--){
+        cin>>a;
+        if(a==1){
+            cin>>b>>c;
+            update_range(1, 1, n, ss[b], ss[b], c);
+        }
+        else{
+            cin>>b;
+            cout<<query(1, 1, n, ss[b], ee[b])<<"\n";
+        }
+    }
+}
+
+
+/*
+for(i=1;i<=n;i++){
+        
+}
+*/
+
+
+/*
+    segment tree
+
+long long init(int node, int st, int end){
+
+    if(st==end) return segt[node]=arr[st];
+    int mid=(st+end)/2;
+
+    return segt[node]=init(node*2, st, mid)+init(node*2+1, mid+1, end);
+
+}
+
+void update(int n, int st, int end, int t, long long diff){
+
+    if(t<st || t>end) return; 
+    segt[n]+=diff;
+
+    if(st==end) return;
+    int mid=(st+end)/2;
+    update(n*2, st, mid, t, diff);
+    update(n*2+1, mid+1, end, t, diff);
+}
+
+long long sum(int l, int r, int node, int st, int end){
+
+    if(l<=st && end<=r) return segt[node];
+    if(r<st || end<l) return 0;
+
+    int m =(st+end)/2;
+    return sum(l, r, node*2, st, m)+sum(l, r, node*2+1, m+1, end);
+
+}*/
+
+/*
+lazy segment tree
+
+void init(int node, int start, int end) {
+    if (start == end) {
+        tree[node] = arr[start];
+    } else {
+        init(node*2, start, (start+end)/2);
+        init(node*2+1, (start+end)/2+1, end);
+        tree[node] = tree[node*2] + tree[node*2+1];
+    }
+}
+void update_lazy(int node, int start, int end) {
+    if (lazy[node] != 0) {
+        tree[node] += (end-start+1)*lazy[node];
+        if (start != end) {
+            lazy[node*2] += lazy[node];
+            lazy[node*2+1] += lazy[node];
+        }
+        lazy[node] = 0;
+    }
+}
+void update_range(int node, int start, int end, int left, int right, long long diff) {
+    update_lazy(node, start, end);
+    if (left > end || right < start) {
+        return;
+    }
+    if (left <= start && end <= right) {
+        tree[node] += (end-start+1)*diff;
+        if (start != end) {
+            lazy[node*2] += diff;
+            lazy[node*2+1] += diff;
+        }
+        return;
+    }
+    update_range(node*2, start, (start+end)/2, left, right, diff);
+    update_range(node*2+1, (start+end)/2+1, end, left, right, diff);
+    tree[node] = tree[node*2] + tree[node*2+1];
+}
+long long query(int node, int start, int end, int left, int right) {
+    update_lazy(node, start, end);
+    if (left > end || right < start) {
+        return 0;
+    }
+    if (left <= start && end <= right) {
+        return tree[node];
+    }
+    long long lsum = query(node*2, start, (start+end)/2, left, right);
+    long long rsum = query(node*2+1, (start+end)/2+1, end, left, right);
+    return lsum + rsum;
+}
+
+
+*/
