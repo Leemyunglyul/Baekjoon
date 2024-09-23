6624675f2e2e38673f1fce78b2b782a0f3d5bde2
Leemyunglyul <140343899+Leemyunglyul@users.noreply.github.com>
2024-09-23 09:08:06 +0900

    [Platinum III] Title: 성대나라의 물탱크, Time: 208 ms, Memory: 39896 KB -BaekjoonHub

--- 백준/Platinum/18227. 성대나라의 물탱크/성대나라의 물탱크.cc
+++ 백준/Platinum/18227. 성대나라의 물탱크/성대나라의 물탱크.cc
@@ -1,64 +1,93 @@
 #include <iostream>
-#include <queue>
-#include <cstring>
-#include <vector>
-#include <algorithm>
-#include <stack>
-#include <map>
 #include <cmath>
-
-#define Min(X, Y) ((X) < (Y) ? (X) : (Y))
-#define Max(X, Y) ((X) > (Y) ? (X) : (Y))
-typedef long long int ll;
+#include <queue>
+#include <vector>
+#include <tuple>
+#include <stack>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <map>
 
 using namespace std;
 
-int N, C, NodeNum = 0;
-vector<int> v[200001];
-int index_tree[200001][2];
-ll tree[1000000];
-ll depth[200001];
+vector<int> edge[201000];
+long long tree[810000];
+long long lazy[810000];
+long long ss[210000], ee[210000], path[210000];
+int cnt=0;
 
-void tree_building(int r){
-    index_tree[r][0] = ++NodeNum;
-    for(int i:v[r]) if(depth[i] == 0) {
-        depth[i] = depth[r] + 1;
-        tree_building(i);
+void dfs(int now, int x){
+    path[now]=x;
+    ss[now]=++cnt;
+    for(int i=0;i<edge[now].size();i++){
+        if(path[edge[now][i]]==0) dfs(edge[now][i], x+1);
     }
-    index_tree[r][1] = NodeNum;
-}
-void update(int d, int l, int r, int x){
-    if(x < l || r < x) return ;
-    tree[d]++;
-    if(l == r) return ;
-    update(2*d, l, (l+r)/2, x);
-    update(2*d+1, (l+r)/2 + 1, r, x);
+    ee[now]=cnt;
+   // cout<<now<<"=="<<ss[now]<<"~~"<<ee[now]<<endl;
 }
 
-ll find(int d, int l, int r, int x, int y){
-    if(y < l || r < x) return 0;
-    if(x <= l && r <= y) return tree[d];
-    return find(2*d, l, (l+r)/2, x, y) + find(2*d+1, (l+r)/2+1, r, x, y);
-}
-
-int main(void){ 
-    memset(tree, 0, sizeof(tree));  
-    memset(depth, 0,sizeof(depth));
-    scanf("%d %d", &N, &C);
-    for(int i=0; i<N-1; i++){
-        int s,e; scanf("%d %d", &s, &e);
-        v[s].push_back(e); v[e].push_back(s);
+void update_lazy(int node, int start, int end) {
+    if (lazy[node] != 0) {
+        tree[node] += lazy[node];
+        if (start != end) {
+            lazy[node*2] += lazy[node];
+            lazy[node*2+1] += lazy[node];
+        }
+        lazy[node] = 0;
     }
-    int k; scanf("%d", &k); depth[C] = 1;
-    tree_building(C); 
+}
+void update_range(int node, int start, int end, int left, int right, long long diff) {
+    update_lazy(node, start, end);
+    if (left > end || right < start) {
+        return;
+    }
+    if (left <= start && end <= right) {
+        tree[node] += diff;
+        if (start != end) {
+            lazy[node*2] += diff;
+            lazy[node*2+1] += diff;
+        }
+        return;
+    }
+    update_range(node*2, start, (start+end)/2, left, right, diff);
+    update_range(node*2+1, (start+end)/2+1, end, left, right, diff);
+    tree[node] = tree[node*2] + tree[node*2+1];
+}
 
-    for(int i=0; i<k; i++){
-        int a, b; scanf("%d %d", &a, &b);
+long long query(int node, int start, int end, int left, int right) {
+    update_lazy(node, start, end);
+    if (left > end || right < start) {
+        return 0;
+    }
+    if (left <= start && end <= right) {
+        return tree[node];
+    }
+    long long lsum = query(node*2, start, (start+end)/2, left, right);
+    long long rsum = query(node*2+1, (start+end)/2+1, end, left, right);
+    return lsum + rsum;
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    long long n, m, i, j, x, a, b, c,r,y;
+    cin>>n>>r;
+    for(i=1;i<=n-1;i++){
+        cin>>x>>y;
+        edge[x].push_back(y);
+        edge[y].push_back(x);
+    }
+    dfs(r, 1);
+    cin>>m;
+    while(m--){
+        cin>>a>>b;
         if(a==1){
-            update(1,1,N,index_tree[b][0]);
-        }else{
-            printf("%lld\n", find(1, 1, N, index_tree[b][0], index_tree[b][1]) * depth[b]);
+            update_range(1, 1, n, ss[b], ss[b], 1);
+        }
+        else{
+            cout<<query(1, 1, n, ss[b], ee[b])*path[b]<<"\n";
         }
     }
-    return 0;
 }
