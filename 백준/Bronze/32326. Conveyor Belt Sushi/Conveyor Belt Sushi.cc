ca9eb1692ccf3b463fb9ca4f0300cea3eac1b35d
Leemyunglyul <140343899+Leemyunglyul@users.noreply.github.com>
2024-09-27 23:24:30 +0900

    [Bronze V] Title: Conveyor Belt Sushi, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32326. Conveyor Belt Sushi/Conveyor Belt Sushi.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+#include <queue>
+#include <vector>
+#include <tuple>
+#include <stack>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <map>
+
+using namespace std;
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    int a, b, c;
+    cin>>a>>b>>c;
+    cout<<a*3+b*4+c*5;
+}
