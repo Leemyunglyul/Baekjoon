9e689617b0aa0df096291d172a87ae2e2f9ba6e8
Leemyunglyul <140343899+Leemyunglyul@users.noreply.github.com>
2024-09-28 21:59:34 +0900

    [Platinum II] Title: Simple, Time: 236 ms, Memory: 35224 KB, Score: 30 point -BaekjoonHub

---
+++ 백준/Platinum/21958. Simple/README.md
@@ -0,0 +1,35 @@
+# [Platinum II] Simple - 21958 
+
+[문제 링크](https://www.acmicpc.net/problem/21958) 
+
+### 성능 요약
+
+메모리: 35224 KB, 시간: 236 ms
+
+### 분류
+
+자료 구조, 느리게 갱신되는 세그먼트 트리, 세그먼트 트리
+
+### 제출 일자
+
+2024년 9월 28일 21:58:50
+
+### 문제 설명
+
+<p>You are given a sequence of N numbers and Q queries:</p>
+
+<ul>
+	<li>0 a b val : the number val will be added to all of the numbers from the interval [a,b]</li>
+	<li>1 a b : you need to print the minimum even number and the maximum odd number form the interval [a,b]; if one of these numbers does not exist, -1 will be printed in its place</li>
+</ul>
+
+<p>Answer all of the type 1 queries.</p>
+
+### 입력 
+
+ <p>The first line contains one integer: N. The second line contains N integers, representing the numbers from the sequence. The third line contains one integer: Q, and the following Q lines contain Q queries, as described in the statement.</p>
+
+### 출력 
+
+ <p>The output contains the answers to all of the type 1 queries, one per line.</p>
+
---
+++ 백준/Platinum/21958. Simple/Simple.cc
@@ -0,0 +1,141 @@
+#include <iostream>
+#include <cmath>
+#include <queue>
+#include <vector>
+#include <tuple>
+#include <stack>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <climits>
+
+using namespace std;
+
+long long arr[200100];
+long long tree[810000][4];
+long long lazy[810000];
+long long big=98765432197654321;
+
+void init(int node, int start, int end) {
+    if (start == end) {
+        if(arr[start]%2==0){
+            tree[node][2]=arr[start];
+            tree[node][3]=arr[start];
+        }
+        else{
+            tree[node][0] = arr[start];
+            tree[node][1]=arr[start];
+        } 
+    } else {
+        init(node*2, start, (start+end)/2);
+        init(node*2+1, (start+end)/2+1, end);
+        tree[node][0] = min(tree[node*2][0], tree[node*2+1][0]);
+        tree[node][1] = max(tree[node*2][1], tree[node*2+1][1]);
+        tree[node][2] = min(tree[node*2][2], tree[node*2+1][2]);
+        tree[node][3] = max(tree[node*2][3], tree[node*2+1][3]);
+    }
+}
+
+void update_lazy(int node, int start, int end) {
+    if (lazy[node] != 0) {
+        if(lazy[node]%2==0){
+            if(tree[node][0]>0){
+                tree[node][0]+=lazy[node];
+                tree[node][1]+=lazy[node];
+            } 
+            if(tree[node][2]<big){
+                tree[node][2]+=lazy[node];
+                tree[node][3]+=lazy[node];
+            } 
+        }
+        else{
+            swap(tree[node][0], tree[node][2]);
+            swap(tree[node][1], tree[node][3]);
+            if(tree[node][0]>=big){
+                tree[node][0]=big;
+                tree[node][1]=0;
+            } 
+            else{
+                tree[node][0]+=lazy[node];
+                tree[node][1]+=lazy[node];
+            }
+            if(tree[node][2]==0){
+                tree[node][2]=big;
+                tree[node][3]=0;
+            }
+            else{
+                tree[node][2]+=lazy[node];
+                tree[node][3]+=lazy[node];
+            } 
+        }
+        if(start!=end){
+            lazy[node*2] += lazy[node];
+            lazy[node*2+1] += lazy[node];
+        }
+        lazy[node] = 0;
+    }
+}
+
+void update_range(int node, int start, int end, int left, int right, long long diff) {
+    if (left > end || right < start) {
+        update_lazy(node, start, end);
+        return;
+    }
+    if (left <= start && end <= right) {
+        lazy[node]+=diff;
+        update_lazy(node, start, end);
+        return;
+    }
+    update_lazy(node, start, end);
+    update_range(node*2, start, (start+end)/2, left, right, diff);
+    update_range(node*2+1, (start+end)/2+1, end, left, right, diff);
+    tree[node][0] = min(tree[node*2][0], tree[node*2+1][0]);
+    tree[node][1] = max(tree[node*2][1], tree[node*2+1][1]);
+    tree[node][2] = min(tree[node*2][2], tree[node*2+1][2]);
+    tree[node][3] = max(tree[node*2][3], tree[node*2+1][3]);
+}
+
+pair<long long, long long>query(int node, int start, int end, int left, int right) {
+    update_lazy(node, start, end);
+    if (left > end || right < start) {
+        return {0, big};
+    }
+    if (left <= start && end <= right) {
+        return {tree[node][1], tree[node][2]};
+    }
+    pair<long long, long long> lsum = query(node*2, start, (start+end)/2, left, right);
+    pair<long long, long long> rsum = query(node*2+1, (start+end)/2+1, end, left, right);
+    return {max(lsum.first, rsum.first), min(lsum.second, rsum.second)};
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    long long n, i, j, m, x,r, a, b, c;
+    cin>>n;
+    for(i=1;i<=4*n;i++){
+        tree[i][0]=big;
+        tree[i][2]=big;
+    }
+    for(i=1;i<=n;i++){
+        cin>>arr[i];
+    }
+    cin>>m;
+    init(1, 1, n);
+    while(m--){
+        cin>>x;
+        if(x==0){
+            cin>>a>>b>>c;
+            update_range(1, 1, n, a, b, c);
+        }
+        else{
+            cin>>a>>b;
+            pair<long long, long long> p=query(1, 1, n, a, b);
+            if(p.second>=big) cout<<"-1 ";
+            else cout<<p.second<<" ";
+            if(p.first<=0) cout<<"-1\n";
+            else cout<<p.first<<"\n";
+           
+        }
+    }
+}
