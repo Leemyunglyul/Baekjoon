dd7ce9b8ecfb06940bbf4d3907cca927af65c4b7
Leemyunglyul <140343899+Leemyunglyul@users.noreply.github.com>
2024-10-01 12:37:04 +0900

    [Silver V] Title: 개미 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/28292. 개미 수열/개미 수열.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+#include <queue>
+#include <vector>
+#include <tuple>
+#include <stack>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <map>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    int n, i, j, anw, sum;
+    cin>>n;
+    if(n<=2) cout<<"1";
+    else if(n<=5) cout<<"2";
+    else cout<<"3";
+}
