0c61e51e9b203448dd38c8e3ac54d984a9a2919b
Leemyunglyul <140343899+Leemyunglyul@users.noreply.github.com>
2024-10-04 10:22:01 +0900

    [Platinum IV] Title: 에바쿰, Time: 128 ms, Memory: 68428 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Platinum/15967. 에바쿰/에바쿰.cc
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <cmath>
+#include <queue>
+#include <vector>
+#include <tuple>
+#include <stack>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <unordered_map>
+typedef long long ll;
+
+using namespace std;
+
+ll tree[4000100];
+ll lazy[4000100];
+int arr[1000100];
+
+void init(int node, int start, int end) {
+    if (start == end) {
+        tree[node] = arr[start];
+    } else {
+        init(node*2, start, (start+end)/2);
+        init(node*2+1, (start+end)/2+1, end);
+        tree[node] = tree[node*2] + tree[node*2+1];
+    }
+}
+void update_lazy(int node, int start, int end) {
+    if (lazy[node] != 0) {
+        tree[node] += (end-start+1)*lazy[node];
+        if (start != end) {
+            lazy[node*2] += lazy[node];
+            lazy[node*2+1] += lazy[node];
+        }
+        lazy[node] = 0;
+    }
+}
+void update_range(int node, int start, int end, int left, int right, long long diff) {
+    update_lazy(node, start, end);
+    if (left > end || right < start) {
+        return;
+    }
+    if (left <= start && end <= right) {
+        tree[node] += (end-start+1)*diff;
+        if (start != end) {
+            lazy[node*2] += diff;
+            lazy[node*2+1] += diff;
+        }
+        return;
+    }
+    update_range(node*2, start, (start+end)/2, left, right, diff);
+    update_range(node*2+1, (start+end)/2+1, end, left, right, diff);
+    tree[node] = tree[node*2] + tree[node*2+1];
+}
+long long query(int node, int start, int end, int left, int right) {
+    update_lazy(node, start, end);
+    if (left > end || right < start) {
+        return 0;
+    }
+    if (left <= start && end <= right) {
+        return tree[node];
+    }
+    long long lsum = query(node*2, start, (start+end)/2, left, right);
+    long long rsum = query(node*2+1, (start+end)/2+1, end, left, right);
+    return lsum + rsum;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    
+    ll n, i, j, x1, y1, y2, x2, m, sum,a, b,c;
+    cin>>n>>x1>>x2;
+    for(i=1;i<=n;i++){
+        cin>>arr[i];
+    }
+    m=x1+x2;
+    init(1, 1, n);
+    while(m--){
+        cin>>i>>a>>b;
+        if(i==1){
+            cout<<query(1, 1, n, a, b)<<"\n";
+        }
+        else{
+            cin>>c;
+            update_range(1, 1, n, a, b, c);
+        }
+    }
+}
