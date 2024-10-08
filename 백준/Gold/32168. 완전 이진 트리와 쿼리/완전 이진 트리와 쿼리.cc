0033f3c1a9731c5395cf8f5a2f8ba71635a37f5d
Leemyunglyul <140343899+Leemyunglyul@users.noreply.github.com>
2024-10-08 09:50:54 +0900

    [Gold I] Title: 완전 이진 트리와 쿼리, Time: 16 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/32168. 완전 이진 트리와 쿼리/완전 이진 트리와 쿼리.cc
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <cmath>
+#include <queue>
+#include <vector>
+#include <tuple>
+#include <stack>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <unordered_map>
+typedef long long ll;
+
+using namespace std;
+
+ll root=1;
+ll n;
+
+inline ll ssum(ll x, ll y){return (x+y)*(y-x+1)/2;} 
+
+ll sub(ll l, ll r){
+    if(l<=n && n<=r) return ssum(l, n);
+    else if(l>n) return 0;
+    else return ssum(l, r)+sub(l*2, r*2+1);
+
+}
+
+bool check(ll l, ll r){
+    if(l<=root && root<=r) return true;
+    else if(l>root) return false;
+    else return check(l*2, r*2+1);
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    
+    ll q, x,y, tot;
+    cin>>n>>q;
+    tot=1+sub(2,3);
+    while(q--){
+        cin>>x>>y;
+        if(x==1) root=y;
+        else{
+            if(y==root){
+                cout<<tot<<"\n";
+                continue;
+            }
+            if(check(4*y, 4*y+1) || 2*y==root){
+                //cout<<"left ";
+                cout<<tot-sub(4*y, 4*y+1)-2*y<<"\n";
+            }
+            else if(check((y*2+1)*2, (y*2+1)*2+1) || 2*y+1==root){
+                //cout<<"right ";
+                cout<<tot-sub((y*2+1)*2, (y*2+1)*2+1)-(2*y+1)<<"\n";
+            }
+            else{
+                //cout<<"else ";
+                cout<<y+sub(2*y, 2*y+1)<<"\n";
+            }
+        }
+    }  
+    
+}
