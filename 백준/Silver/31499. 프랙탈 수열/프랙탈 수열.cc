789959f5e5676e549e82c5759238e35ba0e4cca1
Leemyunglyul <140343899+Leemyunglyul@users.noreply.github.com>
2024-10-17 08:36:36 +0900

    [Silver III] Title: 프랙탈 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/31499. 프랙탈 수열/프랙탈 수열.cc
+++ 백준/Silver/31499. 프랙탈 수열/프랙탈 수열.cc
@@ -1,28 +1,9 @@
 #include <iostream>
-#include <cmath>
-#include <queue>
-#include <vector>
-#include <tuple>
-#include <stack>
-#include <set>
-#include <algorithm>
-#include <string>
-#include <map>
-#include <climits>
-#include <unordered_map>
-typedef long long ll;
-
 using namespace std;
-
-
-
 int main() {
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-
-    ll n, i, k, x;
+    long long n, i, k, x;
     cin>>n>>k;
-    for(i=1, x=1;i<=n;i++){
-        x=x*i%k;
-    }
+    for(i=1, x=1;i<=n;i++) x=x*i%k;
     cout<<x;
 }
