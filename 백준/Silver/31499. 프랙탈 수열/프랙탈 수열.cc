0ba0975ecb8f54bc415003ebe33544411d09301b
Leemyunglyul <140343899+Leemyunglyul@users.noreply.github.com>
2024-10-17 08:37:10 +0900

    [Silver III] Title: 프랙탈 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/31499. 프랙탈 수열/프랙탈 수열.cc
+++ 백준/Silver/31499. 프랙탈 수열/프랙탈 수열.cc
@@ -1,9 +1,7 @@
 #include <iostream>
-using namespace std;
 int main() {
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
     long long n, i, k, x;
-    cin>>n>>k;
+    std::cin>>n>>k;
     for(i=1, x=1;i<=n;i++) x=x*i%k;
-    cout<<x;
+    std::cout<<x;
 }
