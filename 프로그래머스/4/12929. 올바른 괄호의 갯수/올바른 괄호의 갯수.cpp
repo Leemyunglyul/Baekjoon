906b46cec04a720029af623835b53c315d3cfa68
Leemyunglyul <140343899+Leemyunglyul@users.noreply.github.com>
2026-09-07 14:02:40 +0900

    [level 4] Title: 올바른 괄호의 갯수, Time: 0.01 ms, Memory: 4.95 MB -BaekjoonHub

---
+++ 프로그래머스/4/12929. 올바른 괄호의 갯수/올바른 괄호의 갯수.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+using namespace std;
+
+int solution(int n) {
+    vector<long long> dp(n + 1);
+    dp[0] = 1;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 0; j < i; j++) {
+            dp[i] += dp[j] * dp[i - 1 - j];
+        }
+    }
+
+    return dp[n];
+}
